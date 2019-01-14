b0699ccf5cbfe781aa9368c69191013ae4b6d380
chainsawriot <chainsawtiney@gmail.com>
2019-01-14 17:02:01 +0100

    use the untar_pkg() helper. Preparing for reading local tarball.

--- R/fetch.R
+++ R/fetch.R
@@ -1,22 +1,37 @@
 #' @import dplyr
 #' @import purrr
 #' @import tibble
-#' @import RSQLite
 NULL
 
-read_src <- function(filename, pkg, cran_tempdir, verbose = FALSE) {
+read_src <- function(file_path, pkg, verbose = FALSE, filter_empty = TRUE) {
     if (verbose) {
-        print(filename)
+        print(file_path)
     }
-    base_dir <- paste0(cran_tempdir, "/", pkg, "/")
-    if (filename %in% c("NAMESPACE", "DESCRIPTION")) {
-        path <- paste0(base_dir, filename)
-    } else {
-        path <- paste0(base_dir, "R/", filename)
+    ## base_dir <- paste0(cran_tempdir, "/", pkg, "/")
+    ## if (filename %in% c("NAMESPACE", "DESCRIPTION")) {
+    ##     path <- paste0(base_dir, filename)
+    ## } else {
+    ##     path <- paste0(base_dir, "R/", filename)
+    ## }
+    content <- readLines(file_path)
+    if (filter_empty) {
+        content <- content[content != ""]
     }
-    content <- readLines(path)
-    tibble(code = content) %>% filter(code != "") %>% mutate(line_num = seq_along(code), filename = filename, pkg = pkg)
+    filename <- tail(strsplit(file_path, "/")[[1]], 1)
+    tibble(code = content) %>% mutate(line_num = seq_along(code), filename = filename, pkg = pkg)
 }
+
+# Helper function to extract tarball and return the paths of all files and directories in the tarball.
+untar_pkg <- function(pkg_path) {
+    content_list <- untar(pkg_path, list = TRUE)
+    dest_tempdir <- tempdir()
+    z <- untar(pkg_path, exdir = dest_tempdir)
+    if (z == 0) {
+        return(paste0(dest_tempdir, "/", content_list))
+    }
+    stop('Untar failed.')
+}
+
 
 #' Fetch the source of a pkg into a tibble
 #'
@@ -28,20 +43,10 @@ read_src <- function(filename, pkg, cran_tempdir, verbose = FALSE) {
 read_cranpkg <- function(pkg, verbose = FALSE) {
     cran_tempdir <- tempdir()
     res <- download.packages(pkg, destdir = cran_tempdir)
-    z <- untar(res[2], exdir = cran_tempdir)
-    all_files <- c(list.files(paste0(cran_tempdir, "/", pkg, "/R"), pattern = '\\.[rR]$'), "DESCRIPTION", "NAMESPACE")
-    pkg_sources <- map_dfr(all_files, read_src, pkg = pkg, cran_tempdir = cran_tempdir, verbose = verbose)
+    extracted_files <- untar_pkg(res[2])
+    target_files <- grep(extracted_files, pattern = '/R/.+\\.[rR]$|/DESCRIPTION$|/NAMESPACE$', value = TRUE)
+    pkg_sources <- map_dfr(target_files, read_src, pkg = pkg, verbose = verbose)
     return(pkg_sources)
 }
 
-#' Dump source code of a package into a SQLite database.
-#'
-#' More information later.
-#' 
-#' @export
-dump_cranpkg <- function(pkg, dbname) {
-    db <- dbConnect(SQLite(), dbname = dbname)
-    pkg_sources <- fetch_pkg_src(pkg)
-    dbWriteTable(conn = db, name = 'code', value = pkg_sources,  append = TRUE)
-    dbDisconnect(db)
-}
+
