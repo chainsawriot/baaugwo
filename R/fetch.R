76bb9b81e7c61e1fbc55529eb1246c8a2a46f2f2
chainsawriot <chainsawtiney@gmail.com>
2019-01-15 05:11:11 +0100

    add read_tarball. readcranpkg uses read_tarball.

--- R/fetch.R
+++ R/fetch.R
@@ -3,22 +3,16 @@
 #' @import tibble
 NULL
 
-read_src <- function(file_path, pkg, verbose = FALSE, filter_empty = TRUE) {
+read_src <- function(file_path, pkg_name, verbose = FALSE, filter_empty = TRUE) {
     if (verbose) {
         print(file_path)
     }
-    ## base_dir <- paste0(cran_tempdir, "/", pkg, "/")
-    ## if (filename %in% c("NAMESPACE", "DESCRIPTION")) {
-    ##     path <- paste0(base_dir, filename)
-    ## } else {
-    ##     path <- paste0(base_dir, "R/", filename)
-    ## }
     content <- readLines(file_path)
     if (filter_empty) {
         content <- content[content != ""]
     }
     filename <- tail(strsplit(file_path, "/")[[1]], 1)
-    tibble(code = content) %>% mutate(line_num = seq_along(code), filename = filename, pkg = pkg)
+    tibble(code = content) %>% mutate(line_num = seq_along(code), filename = filename, pkg_name = pkg_name)
 }
 
 # Helper function to extract tarball and return the paths of all files and directories in the tarball.
@@ -33,9 +27,10 @@ untar_pkg <- function(pkg_path) {
 }
 
 
-#' Fetch the source of a pkg into a tibble
+#' Fetch the source of a pkg and dump them into a tibble
 #'
-#' This function fetches the source code of a package and dump them into a tibble.
+#' This function fetches the source code of a package from CRAN and dump them into a tibble.
+#' 
 #' @param pkg string, name of a package.
 #' @param verbose boolean, display debug info.
 #' @return a tibble with the source code of all R files, DESCRIPTION and NAMESPACE
@@ -43,10 +38,22 @@ untar_pkg <- function(pkg_path) {
 read_cranpkg <- function(pkg, verbose = FALSE) {
     cran_tempdir <- tempdir()
     res <- download.packages(pkg, destdir = cran_tempdir)
-    extracted_files <- untar_pkg(res[2])
-    target_files <- grep(extracted_files, pattern = '/R/.+\\.[rR]$|/DESCRIPTION$|/NAMESPACE$', value = TRUE)
-    pkg_sources <- map_dfr(target_files, read_src, pkg = pkg, verbose = verbose)
-    return(pkg_sources)
+    return(read_tarball(res[2], verbose = verbose))
 }
 
-
+#' Dump the source from a package tarball into a tibble
+#'
+#' This function fetches the source code of a package from CRAN and dump them into a tibble.
+#' 
+#' @param tarball_path string, path to a tarball in .tar.gz.
+#' @param verbose boolean, display debug info.
+#' @return a tibble with the source code of all R files, DESCRIPTION and NAMESPACE
+#' @export
+read_tarball <- function(tarball_path, verbose = FALSE) {
+    extracted_files_paths <- untar_pkg(tarball_path)
+    target_files_paths <- grep(extracted_files_paths, pattern = '/R/.+\\.[rR]$|/DESCRIPTION$|/NAMESPACE$', value = TRUE)
+    parsed_description <- read.dcf(grep(target_files_paths, pattern = "/DESCRIPTION$", value = TRUE))
+    pkg_name <- as.character(parsed_description[,1]) ## this doesn't feel safe.
+    pkg_sources <- map_dfr(target_files_paths, read_src, pkg_name = pkg_name, verbose = verbose)
+    return(pkg_sources)
+}
